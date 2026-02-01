7672d1bf1bbc250921a20f8596f28e575433a626
jeettan <jeetta3@gmail.com>
2026-02-01 15:35:05 +0700

    Added mongodb cloud

--- startup/db.js
+++ startup/db.js
@@ -6,6 +6,7 @@ module.exports = function () {
     mongoose
         .connect(`${config.get('db')}`)
         .then(() => {
+            console.log(mongoose.connection.name)
             console.log(`Successfully connected to ${config.get('db')}`);
         })
         .catch((err) => console.error(err));
